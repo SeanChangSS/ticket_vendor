8d5ceab9c342ff5652d5d46f10d0c4db40f52d6a
SeanChangSS <ina@cloudybay.com.tw>
2020-08-20 18:05:48 +0800

    add model, remove pyc

--- ticket_vendor/change_money/models.py
+++ ticket_vendor/change_money/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class TransactionRecord(models.Model):
+    price = models.IntegerField(verbose_name="總金額")
+    amount = models.IntegerField(verbose_name="票卷張數")
+    purchase_date = models.DateTimeField(auto_now_add=True, verbose_name="購買日期與時間")
--- ticket_vendor/urls.py
+++ ticket_vendor/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from ticket_vendor.change_money import urls as change_money
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('change_money/', include(change_money))
 ]
